e5769b99505fe0456b7b85286b15332934c5c557
Marco García <marcoantonio.garcia@wundermanthompson.com>
2021-07-06 22:21:39 -0600

    Primer commit

---
+++ app/Http/Controllers/SubscriberController.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Subscriber;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class SubscriberController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        echo "Hola";
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $rules = array(
+            'name' => 'required',
+            'email' => 'required|unique:subscribers|email',
+        );
+        $messages = array(
+            'name.required' => 'Por favor ingresa un nombre',
+            'email.required' => 'Por favor ingresa un correo electrónico.',
+            'email.unique' => 'Lo sentimos este correo electrónico ya fue registrado.',
+            'email.email' => 'Formato de correo electrónico incorrecto.'
+        );
+
+        $validator = Validator::make($request->all(), $rules, $messages);
+        if ($validator->fails()) {
+            return response()->json(["errors" => $validator->errors()], 400);
+        } else {
+            $subscriber = Subscriber::create($request->all());
+            return response()->json($subscriber, 200);
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Subscriber  $subscriber
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Subscriber $subscriber)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Subscriber  $subscriber
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Subscriber $subscriber)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Subscriber  $subscriber
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Subscriber $subscriber)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Subscriber  $subscriber
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Subscriber $subscriber)
+    {
+        //
+    }
+}
